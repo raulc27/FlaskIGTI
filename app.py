81473ca612dfd40543df38b9c3188360ece21059
Raul Castro <raulc27@gmail.com>
2021-11-23 00:45:12 -0300

    configurando alchemy

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['PROPAGATE_EXCEPTIONS']=True
+
 @app.route('/', methods=['GET'])
 def home():
     return "API funcionando...", 200
